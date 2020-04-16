65063c8a4725f9e3b12681c454d7c4400bf327cd
ekembe <ekembe.thierry@gmail.com>
2020-04-16 14:56:08 +0200

    creation de la fonction faisant l'estimation de l'impact du covid 19 et du composant react permettant la saisie de données et l'affchage des résultats

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,63 @@
-const covid19ImpactEstimator = (data) => data;
+const periodUnits = {"days": 1, "weeks": 7, "months": 30};
+const progressionRatio = 3;
+
+export const covid19ImpactEstimator = (data) => {
+	const input = data;
+	return {
+		"data": input,
+		"impact": computeImpact(data),
+		"severeImpact": computeSevereImpact(data)
+	}
+};
+
+function computeImpact(data){
+	let currentlyInfected = data.reportedCases * 10;
+	
+	let infectionsByRequestedTime = predictInfections(currentlyInfected, data.timeToElapse, periodUnits[data.periodType]);
+	
+	let dollarsInFlight = economyLossByRequestedTime(infectionsByRequestedTime, data.timeToElapse, periodUnits[data.periodType], data.region.avgDailyIncomePopulation, data.region.avgDailyIncomeInUSD);
+	
+	return {
+		'currentlyInfected' : currentlyInfected,
+		'infectionsByRequestedTime' : infectionsByRequestedTime,
+		'dollarsInFlight' : dollarsInFlight
+	};
+}
+
+function computeSevereImpact(data){
+	let currentlyInfected = data.reportedCases * 50;
+	
+	let infectionsByRequestedTime = predictInfections(currentlyInfected, data.timeToElapse, periodUnits[data.periodType])
+	
+	let severeCasesByRequestedTime = Math.trunc(0.15 * infectionsByRequestedTime);
+	
+	let availableHospitalBeds = Math.trunc(0.35 * data.totalHospitalBeds);
+	let hospitalBedsByRequestedTime = availableHospitalBeds - severeCasesByRequestedTime;
+	 
+	let casesForICUByRequestedTime =  Math.trunc(0.05 * infectionsByRequestedTime);
+	
+	let casesForVentilatorsByRequestedTime = Math.trunc(0.02 * infectionsByRequestedTime);
+	
+	let dollarsInFlight = economyLossByRequestedTime(infectionsByRequestedTime, data.timeToElapse, periodUnits[data.periodType], data.region.avgDailyIncomePopulation, data.region.avgDailyIncomeInUSD);
+	
+	return {
+		'currentlyInfected' : currentlyInfected,
+		'infectionsByRequestedTime' : infectionsByRequestedTime,
+		'severeCasesByRequestedTime' : severeCasesByRequestedTime,
+		'hospitalBedsByRequestedTime' : hospitalBedsByRequestedTime,
+		'casesForICUByRequestedTime': casesForICUByRequestedTime,
+		'casesForVentilatorsByRequestedTime' : casesForVentilatorsByRequestedTime,
+		'dollarsInFlight' : dollarsInFlight
+	};
+}
+
+function predictInfections(currentlyInfected, timeValue, timeUnit){
+	let factor = Math.trunc(timeValue * timeUnit / progressionRatio);
+	return currentlyInfected * 2 ** factor;
+}
+
+function economyLossByRequestedTime(infectionsByRequestedTime, timeValue, timeUnit, adip, adiiu){
+	return Math.trunc(infectionsByRequestedTime * adip * adiiu * timeValue * timeUnit * 100) / 100;
+}
 
 export default covid19ImpactEstimator;
